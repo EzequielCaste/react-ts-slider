6b2068688156ba751c90fad5b21d0366490f29ab
EzequielCaste <ezequiel.caste@gmail.com>
2021-07-19 21:54:34 -0300

    slider working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import {setInterval} from 'timers';
 import './App.css';
 
-function App() {
+import reviews from './reviews.json';
+
+const App: React.FC = () => {
+  const [active, setActive] = useState(0);
+  let timer: NodeJS.Timer;
+
+  if (active > 2) {
+    setActive(0);
+  }
+
+  const nextSlide = () => {
+    if (active > 2) {
+      setActive(0);
+    } else {
+      setActive((prev) => prev + 1);
+    }
+  };
+  const prevSlide = () => {
+    if (active === 0) {
+      setActive(2);
+    } else {
+      setActive((prev) => prev - 1);
+    }
+  };
+
+  const changeSlide = () => {
+    timer = setInterval(() => {
+      active < 2 ? nextSlide() : setActive(0);
+    }, 5000);
+  };
+
+  // useEffect(() => {
+  //   changeSlide();
+
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>
+        <span>/</span> Reviews
+      </h1>
+      <div className="slider-container">
+        <article
+          className={
+            active === 0
+              ? 'active-slide'
+              : active === 2
+              ? 'next-slide'
+              : 'prev-slide'
+          }
         >
-          Learn React
-        </a>
-      </header>
+          <img src={reviews[0].image} alt={reviews[0].name} />
+          <div className="info">
+            <div className="name">{reviews[0].name}</div>
+            <div className="title">{reviews[0].title}</div>
+            <div className="quote">{reviews[0].quote}</div>
+          </div>
+        </article>
+        <article
+          className={
+            active === 1
+              ? 'active-slide'
+              : active + 1 === 1
+              ? 'next-slide'
+              : 'prev-slide'
+          }
+        >
+          <img src={reviews[1].image} alt={reviews[1].name} />
+          <div className="info">
+            <div className="name">{reviews[1].name}</div>
+            <div className="title">{reviews[1].title}</div>
+            <div className="quote">{reviews[1].quote}</div>
+          </div>
+        </article>
+        <article
+          className={
+            active === 2
+              ? 'active-slide'
+              : active + 1 === 2
+              ? 'next-slide'
+              : 'prev-slide'
+          }
+        >
+          <img src={reviews[2].image} alt={reviews[2].name} />
+          <div className="info">
+            <div className="name">{reviews[2].name}</div>
+            <div className="title">{reviews[2].title}</div>
+            <div className="quote">{reviews[2].quote}</div>
+          </div>
+        </article>
+
+        <div onClick={prevSlide} className="prev">
+          {'<'}
+        </div>
+        <div onClick={nextSlide} className="next">
+          {'>'}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
